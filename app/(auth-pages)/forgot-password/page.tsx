7d6ee2e08d5ad80b6a80e62992635f134e7d5d1c
sebiten <sebdevspace@gmail.com>
2025-03-24 21:51:55 -0300

    form-message component recovery

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -1,10 +1,10 @@
 import { forgotPasswordAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
+import { FormMessage, Message } from "@/components/form-message";
 
 export default async function ForgotPassword(props: {
   searchParams: Promise<Message>;
